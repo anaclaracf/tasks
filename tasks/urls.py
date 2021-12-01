698ea06c04dff9419e5a76774df7d01675ffc4d5
Ana Clara Carneiro de Freitas <anaclara.2001@hotmail.com>
2021-12-01 11:39:40 -0300

    tasks implementada

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
-
+from django.urls import include, path
+from rest_framework import routers
 from . import views
+# from views import TaskViewSet
 
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import TaskSerializer
+from .models import Task
 
 
 # Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 
 def index(request):
