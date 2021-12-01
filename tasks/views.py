6f275c6aac094e1a7df7ac71c2b77d8d8b28415c
Ana Clara Carneiro de Freitas <anaclara.2001@hotmail.com>
2021-12-01 16:37:23 -0300

    erros

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework import viewsets
-from rest_framework import permissions
-from .serializers import TaskSerializer
+from django.http import HttpResponse
 from .models import Task
+from .serializer import TaskSerializer
 from rest_framework import status
-from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from django.forms.models import model_to_dict
+from rest_framework.response import Response
 from django.core import serializers
 
 # Create your views here.
@@ -17,30 +13,29 @@ def index(request):
 
 @api_view(['GET'])
 def get_all_tasks(request):
-    all_tasks = Task.object.all()
-    # serializer = TaskSerializer(all_tasks)
+    all_tasks = Task.objects.all()
     json_response = serializers.serialize("json", all_tasks)
-    return HttpResponse(json_response, content_type="application/json")
+    return HttpResponse(json_response, content_type="application/json", status=status.HTTP_201_CREATED)
 
-@api_view(['POST', 'DELETE'])
-def task_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
-    try:
-        task = Task.objects.get(pk=pk)
+# @api_view(['POST', 'DELETE'])
+# def task_detail(request, pk):
+#     """
+#     Retrieve, update or delete a code snippet.
+#     """
+#     try:
+#         task = Task.objects.get(pk=pk)
         
-    except Task.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+#     except Task.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'POST':
-        serializer = TaskSerializer(task, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return HttpResponse(request.data,  content_type="application/json", status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     if request.method == 'POST':
+#         serializer = TaskSerializer(task, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return HttpResponse(request.data,  content_type="application/json", status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
-        task.delete()
-        json_response = serializers.serialize("json", task)
-        return HttpResponse(json_response, content_type="application/json", status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == 'DELETE':
+#         task.delete()
+#         json_response = serializers.serialize("json", task)
+#         return HttpResponse(json_response, content_type="application/json", status=status.HTTP_204_NO_CONTENT)
